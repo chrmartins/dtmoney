f3c8c190cef66c9f71fab999d77d44c6ba1ee3b0
Christian Martins <cmts2010@gmail.com>
2021-03-29 18:19:53 -0300

    feat: transaction table

---
+++ src/components/Dashbord/index.tsx
@@ -0,0 +1,14 @@
+﻿import { Summary } from "../Summary";
+import { TransactionTable } from "../TransactionTable";
+import { Container } from "./styles";
+
+
+export function Dashboard() {
+  return (
+    <Container>
+      <Summary />
+      <TransactionTable />
+    </Container>
+
+  )
+}
